13631a61eee4566de3bfdbf3fd46e0d1a3b09f3a
Christopher Hall <christopher.hall.3609@gmail.com>
2016-08-27 17:41:16 +1000

    Added keyboard & mouse input

---
+++ Bipolar-core/src/bplr/input/Mouse.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <glfw3.h>
+#include "../graphics/Window.h"
+#include "../math/vec2.h"
+
+namespace bplr
+{
+	namespace input
+	{
+		class Mouse
+		{
+		public:
+			static void mouseMoveCallback(GLFWwindow* window, double xPosition, double yPosition);
+
+			static math::vec2 getPosition();
+			static math::vec2 getMovement();
+
+			static void setCaptured(graphics::window* window, bool value);
+			static void toggleCaptured(graphics::window* window); 
+			static bool isCaptured();
+
+			static void update();
+
+		private:
+			static math::vec2 m_position;
+			static math::vec2 m_difference;
+			static bool m_captured;
+		};
+	}
+}
