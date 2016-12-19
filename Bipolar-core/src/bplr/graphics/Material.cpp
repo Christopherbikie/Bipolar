768167cf57a1bc7be7ffbddabb9f6c69017af476
Christopher Hall <christopher.hall.3609@gmail.com>
2016-12-19 21:27:44 +1100

    Material usingXMap values are correctly set

--- Bipolar-core/src/bplr/graphics/Material.cpp
+++ Bipolar-core/src/bplr/graphics/Material.cpp
@@ -25,24 +25,32 @@ namespace bplr
 				m_albedoMap->bind(shader, (structName + ".albedoMap").c_str());
 				shader->loadUniform((structName + ".usingAlbedoMap").c_str(), 1.0f);
 			}
+			else
+				shader->loadUniform((structName + ".usingAlbedoMap").c_str(), 0.0f);
 			if (m_glossMap != nullptr)
 			{
 				glActiveTexture(GL_TEXTURE1);
 				m_glossMap->bind(shader, (structName + ".glossMap").c_str());
 				shader->loadUniform((structName + ".usingGlossMap").c_str(), 1.0f);
 			}
+			else
+				shader->loadUniform((structName + ".usingGlossMap").c_str(), 0.0f);
 			if (m_normalMap != nullptr)
 			{
 				glActiveTexture(GL_TEXTURE2);
 				m_normalMap->bind(shader, (structName + ".normalMap").c_str());
 				shader->loadUniform((structName + ".usingNormalMap").c_str(), 1.0f);
 			}
+			else
+				shader->loadUniform((structName + ".usingNormalMap").c_str(), 0.0f);
 			if (m_specularMap != nullptr)
 			{
 				glActiveTexture(GL_TEXTURE3);
 				m_specularMap->bind(shader, (structName + ".specularMap").c_str());
 				shader->loadUniform((structName + ".usingSpecularMap").c_str(), 1.0f);
 			}
+			else
+				shader->loadUniform((structName + ".usingSpecularMap").c_str(), 0.0f);
 
 			shader->loadUniform(structName + ".albedo", m_albedo);
 			shader->loadUniform(structName + ".specular", m_specular);
